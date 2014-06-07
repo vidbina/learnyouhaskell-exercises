c59c92511069921ca3833b125bd6d2e4e2fb14c9
David Asabina <david@supr.nu>
2014-06-07 21:15:30 +0200

    Factorial f(x)'s in Haskell look soo pretty

--- baby.hs
+++ baby.hs
@@ -2,3 +2,4 @@ doubleMe x = x*2
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
 doubleSmallNumber' x = doubleSmallNumber x + 1
+factorial x = product [1..x]
