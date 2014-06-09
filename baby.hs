e36f8f89b04c58e849c54a50f428428dd7fa35f2
David Asabina <david@supr.nu>
2014-06-10 01:57:21 +0200

    First stab at playing with type declarations

--- baby.hs
+++ baby.hs
@@ -2,4 +2,13 @@ doubleMe x = x*2
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
 doubleSmallNumber' x = doubleSmallNumber x + 1
+
+factorial :: Integer -> Integer
 factorial x = product [1..x]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+-- Isn't there a way to use the same implementation for different type declarations?
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
