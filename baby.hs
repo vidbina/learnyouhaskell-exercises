67551f07bf3e8cc1ee0627d81e1e32f33aa499fc
David Asabina <david@supr.nu>
2014-06-10 02:25:03 +0200

    Pattern matching and recursion to calc factorials + learned I was lucky that produc [1..x] worked, as I was unaware of its behaviour at an empty list

--- baby.hs
+++ baby.hs
@@ -3,8 +3,9 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
 doubleSmallNumber' x = doubleSmallNumber x + 1
 
-factorial :: Integer -> Integer
-factorial x = product [1..x]
+factorial :: Integral a => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
